b6de50d10581a64dd3a1f419d0512b843a06f507
MEHSOYY <72802928+MEHSOYY@users.noreply.github.com>
2025-03-24 22:46:52 -0400

    Repository.java

--- src/main/java/academy/javapro/week9/lab/Repository.java
+++ src/main/java/academy/javapro/week9/lab/Repository.java
@@ -4,56 +4,45 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 /**
- * A generic repository for storing and retrieving items of type T.
- * @param <T> the type of items stored in this repository
+ * Generic repository for storing and managing collections of items.
+ * @param <T> the type of items to store
  */
 public class Repository<T> {
-    // TODO: Add a private final List field called items that stores items of type T.
-    /**
-     * Constructs a new empty repository.
-     */
-    public Repository() {
-        // TODO: Initialize the items field with a new ArrayList.
-    }
+    private final List<T> items = new ArrayList<>();
 
     /**
      * Adds an item to the repository.
-     * @param item the item to add
-     * @return true if the item was added successfully
+     * @param item the item to add (cannot be null)
      * @throws IllegalArgumentException if the item is null
      */
-    public boolean add(T item) {
-        // TODO: Throw an IllegalArgumentException with the message "Item cannot be null" if the item is null.
-
-        // TODO: Add the item to the items list and return the result.
-        throw new UnsupportedOperationException("Method not implemented");
+    public void add(T item) {
+        if (item == null) {
+            throw new IllegalArgumentException("Item cannot be null");
+        }
+        items.add(item);
     }
 
     /**
      * Removes an item from the repository.
      * @param item the item to remove
-     * @return true if the item was removed successfully
+     * @return true if the item was removed, false otherwise
      */
     public boolean remove(T item) {
-        // TODO: Remove the item from the items list and return the result.
-        throw new UnsupportedOperationException("Method not implemented");
+        return items.remove(item);
     }
 
     /**
-     * Finds items that match the given predicate.
+     * Finds items matching the given predicate.
      * @param predicate the condition to match
-     * @return a list of matching items
-     * @throws IllegalArgumentException if the predicate is null
+     * @return a list of matching items (never null)
      */
     public List<T> find(Predicate<T> predicate) {
         if (predicate == null) {
-            throw new IllegalArgumentException("Predicate cannot be null");
+            return Collections.emptyList();
         }
-        // TODO:  Return a list of items using stream().filter(predicate).collect(Collectors.toList()) on the items list.
-        throw new UnsupportedOperationException("Method not implemented");
+        return items.stream().filter(predicate).toList();
     }
 
     /**
@@ -61,26 +50,23 @@ public class Repository<T> {
      * @return an unmodifiable list of all items
      */
     public List<T> getAll() {
-        // TODO: Return an unmodifiable list of items using Collections.unmodifiableList(items).
-        throw new UnsupportedOperationException("Method not implemented");
+        return Collections.unmodifiableList(items);
     }
 
     /**
      * Counts the number of items in the repository.
-     * @return the count of items
+     * @return the number of items
      */
     public int count() {
-        // TODO: Return the size of the items list.
-        throw new UnsupportedOperationException("Method not implemented");
+        return items.size();
     }
 
     /**
-     * Checks if an item is in the repository.
+     * Checks if the repository contains an item.
      * @param item the item to check
-     * @return true if the repository contains the item
+     * @return true if the item is in the repository, false otherwise
      */
     public boolean contains(T item) {
-        // TODO: Return true if the items list contains the item.
-        throw new UnsupportedOperationException("Method not implemented");
+        return items.contains(item);
     }
 }
