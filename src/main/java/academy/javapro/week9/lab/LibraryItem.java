b9347c0a02743dabf81890463b0028c38395790e
MEHSOYY <72802928+MEHSOYY@users.noreply.github.com>
2025-03-24 22:46:07 -0400

    LibraryItem.java

--- src/main/java/academy/javapro/week9/lab/LibraryItem.java
+++ src/main/java/academy/javapro/week9/lab/LibraryItem.java
@@ -1,24 +1,24 @@
 package academy.javapro.week9.lab;
 
 /**
- * Interface for library items with common methods for all library items.
+ * Interface representing a library item with common behavior.
  */
 public interface LibraryItem {
     /**
-     * Gets the title of the library item.
-     * @return the title as a String
+     * Gets the title of the item.
+     * @return the title of the item
      */
-    // TODO: Define the getTitle method that returns a String.
+    String getTitle();
 
     /**
-     * Gets the type of the library item (e.g., "Book", "Magazine").
-     * @return the item type as a String
+     * Gets the type of the item.
+     * @return the type of the item
      */
-    // TODO: Define the getItemType method that returns a String.
+    String getItemType();
 
     /**
-     * Gets a unique identifier for the library item.
-     * @return the unique identifier as a String
+     * Gets a unique identifier for the item.
+     * @return the unique identifier
      */
-    // TODO: Define the getUniqueIdentifier method that returns a String.
+    String getUniqueIdentifier();
 }
